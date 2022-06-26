d0a1ce24052c80cf73ff69e1e6757b50b6dcd1e9
Daniel Rivera <dr734659@gmail.com>
2022-06-25 21:45:31 -0500

    [ADD] PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
-module.exports = {
+const withPwa= require("next-pwa");
+const runtimeCaching= require("next-pwa/cache");
+
+module.exports= withPwa({
+    pwa: {
+        dest:"public",
+        register: true,
+        mode: "production",
+        disable: false,
+        runtimeCaching,
+        buildExcludes: [/middleware-manifest\.json$/],
+    },
     reactStrictMode: true,
     images: {
         domains: ["api.lorem.space"],
         unoptimized: true,
-    },
+    }
+})
+
+// module.exports = {
+    
+//     },
     // distDir: "build",
     // sassOptions: {
     //     includePaths: [path.join(__dirname, 'styles')],
@@ -23,4 +39,4 @@ module.exports = {
     //         }
     //     ]
     // }
-}
+// }
