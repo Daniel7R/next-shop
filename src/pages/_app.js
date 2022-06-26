4f1e6175b28ac8b53e2abf42871a833d3e453d19
Daniel Rivera <dr734659@gmail.com>
2022-06-25 19:54:41 -0500

    [ADD] Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { AppContext } from '@context/AppContext';
 import { useInitialState } from '@hooks/useInitialState';
 import { Header } from "@components/Header";
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({Component, pageProps}) {
     const initialState= useInitialState();
@@ -12,6 +13,16 @@ function MyApp({Component, pageProps}) {
         <Head>    
             <link rel="shortcut icon" href="/favicon_yard_sale.svg" />
         </Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KGQ41PY7L0" strategy='afterInteractive' />
+        <Script id='google-analytics' strategy='afterInteractive'>
+            {`                   
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-KGQ41PY7L0');
+            `}
+        </Script>
         <Header />
         <Component {...pageProps} />
     </AppContext.Provider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
         return(
             <Html lang="es">
                 <Head>
-                    
+                    <meta charSet="utf-8" />
                 </Head>
                 <body>
                     <Main />
